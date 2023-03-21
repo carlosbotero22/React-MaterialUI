f0b58b6da1ea1579612a021b6600b9058242a4a7
carlosbotero22 <monoprograma22@gmail.com>
2023-03-21 14:06:33 -0500

    Grid - Responsive layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,94 @@
-import { Button, Container, Typography } from "@mui/material"
-import { Box } from "@mui/system"
-import CurrencyBitcoinIcon from '@mui/icons-material/CurrencyBitcoin';
-import SendIcon from '@mui/icons-material/Send';
-
+import { Button, Container, Typography, Grid } from "@mui/material";
+import { Box } from "@mui/system";
+import CurrencyBitcoinIcon from "@mui/icons-material/CurrencyBitcoin";
+import SendIcon from "@mui/icons-material/Send";
 
 export default function App() {
   return (
-    <Container sx={{border: 2, boxShadow: 2, pb: 2}}>
-      {/* tipografia */}
-      <h1>React + Vite</h1>
-      <Typography variant="h1">Texto 1</Typography>
-      <Typography variant="h1" component="span" textAlign="center">Texto 1</Typography>
+    <Container>
+      <Grid container spacing={5}>
+        <Grid item xs={12} sm={6} md={4}>
+          {/* tipografia */}
+          <h1>React + Vite</h1>
+          <Typography variant="h1">Texto 1</Typography>
+          <Typography variant="h1" component="span" textAlign="center">
+            Texto 1
+          </Typography>
+
+          {/* BOX COMPONENT */}
+          <Box sx={{ border: 2, padding: 2, bgcolor: "black", color: "white" }}>
+            componente box
+          </Box>
+
+          {/* botones */}
+          <Button
+            variant="contained"
+            color="error"
+            startIcon={<CurrencyBitcoinIcon />}
+          >
+            PRUEBA
+          </Button>
+          <Button variant="contained" color="success" endIcon={<SendIcon />}>
+            PRUEBA
+          </Button>
+          <Button variant="outlined">PRUEBA</Button>
+        </Grid>
+
+        <Grid item xs={12} sm={6} md={4}>
+          {/* tipografia */}
+          <h1>React + Vite</h1>
+          <Typography variant="h1">Texto 1</Typography>
+          <Typography variant="h1" component="span" textAlign="center">
+            Texto 1
+          </Typography>
+
+          {/* BOX COMPONENT */}
+          <Box sx={{ border: 2, padding: 2, bgcolor: "black", color: "white" }}>
+            componente box
+          </Box>
+
+          {/* botones */}
+          <Button
+            variant="contained"
+            color="error"
+            startIcon={<CurrencyBitcoinIcon />}
+          >
+            PRUEBA
+          </Button>
+          <Button variant="contained" color="success" endIcon={<SendIcon />}>
+            PRUEBA
+          </Button>
+          <Button variant="outlined">PRUEBA</Button>
+        </Grid>
+
+        <Grid item xs={12} sm={12} md={4}>
+          {/* tipografia */}
+          <h1>React + Vite</h1>
+          <Typography variant="h1">Texto 1</Typography>
+          <Typography variant="h1" component="span" textAlign="center">
+            Texto 1
+          </Typography>
+
+          {/* BOX COMPONENT */}
+          <Box sx={{ border: 2, padding: 2, bgcolor: "black", color: "white" }}>
+            componente box
+          </Box>
+
+          {/* botones */}
+          <Button
+            variant="contained"
+            color="error"
+            startIcon={<CurrencyBitcoinIcon />}
+          >
+            PRUEBA
+          </Button>
+          <Button variant="contained" color="success" endIcon={<SendIcon />}>
+            PRUEBA
+          </Button>
+          <Button variant="outlined">PRUEBA</Button>
+        </Grid>
+      </Grid>
       
-
-      {/* BOX COMPONENT */}
-      <Box sx={{border: 2, padding: 2, bgcolor: "black", color: "white"}}>componente box</Box>
-
-      {/* botones */}
-      <Button variant="contained" color="error" startIcon={<CurrencyBitcoinIcon />}>PRUEBA</Button>
-      <Button variant="contained" color="success" endIcon={<SendIcon />}>PRUEBA</Button>
-      <Button variant="outlined">PRUEBA</Button>
     </Container>
-  )
+  );
 }
