f764cdadd313be88280ca395adfc2924554fec33
carlosbotero22 <monoprograma22@gmail.com>
2023-03-21 10:58:38 -0500

    instalando Vite + React - material UI y probando botones y otros

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Button, Container } from "@mui/material"
+
+export default function App() {
+  return (
+    <Container sx={{border: 2, boxShadow: 2, pb: 2}}>
+      <h1>holaaaa</h1>
+      <Button variant="contained">PRUEBA</Button>
+    </Container>
+  )
+}
