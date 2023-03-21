9422efa65394c3463b165fefbd46078bff466bf3
carlosbotero22 <monoprograma22@gmail.com>
2023-03-21 11:26:29 -0500

    Box Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Button, Container, Typography } from "@mui/material"
+import { Box } from "@mui/system"
 
 export default function App() {
   return (
     <Container sx={{border: 2, boxShadow: 2, pb: 2}}>
-      <h1>holaaaa</h1>
+      {/* tipografia */}
+      <h1>React + Vite</h1>
       <Typography variant="h1">Texto 1</Typography>
       <Typography variant="h1" component="span" textAlign="center">Texto 1</Typography>
-      
       <Button variant="contained">PRUEBA</Button>
+
+      {/* BOX COMPONENT */}
+      <Box sx={{border: 2, padding: 2, bgcolor: "black", color: "white"}}>componente box</Box>
     </Container>
   )
 }
