f37eb784c3b027c42869cbc0c57c47027e6e6ff6
carlosbotero22 <monoprograma22@gmail.com>
2023-03-21 11:40:00 -0500

    theming

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,25 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
 import './index.css'
-import { CssBaseline } from '@mui/material';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#001427",
+    },
+    secundary: {
+      main: "#b500d6",
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CssBaseline/>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
