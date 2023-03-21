f2359380eeac404be97c70ddc3124f4083bdb768
carlosbotero22 <monoprograma22@gmail.com>
2023-03-21 11:10:47 -0500

    Tipografia y System Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { Button, Container } from "@mui/material"
+import { Button, Container, Typography } from "@mui/material"
 
 export default function App() {
   return (
     <Container sx={{border: 2, boxShadow: 2, pb: 2}}>
       <h1>holaaaa</h1>
+      <Typography variant="h1">Texto 1</Typography>
+      <Typography variant="h1" component="span" textAlign="center">Texto 1</Typography>
+      
       <Button variant="contained">PRUEBA</Button>
     </Container>
   )
